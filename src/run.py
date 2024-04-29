7905b2707a292121096ddba1fe6902ffcdb20d9b
seb2o <87731987+seb2o@users.noreply.github.com>
2024-04-29 18:18:28 +0200

    cleaned code

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')
+model = YOLO('../yolov8n.pt')
 model.predict(
    source='https://media.roboflow.com/notebooks/examples/dog.jpeg',
    conf=0.25
