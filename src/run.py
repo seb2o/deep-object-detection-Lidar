eb1c266209409accae795957b0738dfd056c8970
seb2o <87731987+seb2o@users.noreply.github.com>
2024-05-01 23:01:07 +0200

    run the training of the model trough py file

--- src/run.py
+++ src/run.py
@@ -1,7 +1,30 @@
 from ultralytics import YOLO
 
-model = YOLO('../yolov8n.pt')
-model.predict(
-   source='https://media.roboflow.com/notebooks/examples/dog.jpeg',
-   conf=0.25
-)
+
+def main():
+    model_tuned = YOLO("yolov8s.pt")
+    model_tuned.to("cuda")
+    model_tuned.train(
+        data='../NAPLab-LiDAR/data.yaml',
+        time=.1,
+        patience=3,
+        batch=-1,
+        imgsz=1024,
+        rect=True,
+        save_period=10,
+        cache='ram',
+        device=0,
+        project='tuned_exp_2',
+        pretrained=False,
+        deterministic=False,
+        plots=True,
+        hsv_h=0,
+        mosaic=False,
+        copy_paste=0.5
+    )
+    model_tuned_metrics = model_tuned.val(data="../NAPLab-LiDAR/test.yaml")
+    print(model_tuned_metrics)
+
+
+if __name__ == '__main__':
+    main()
