c876afca06d8806aca994c15caab158a5349571a
seb2o <87731987+seb2o@users.noreply.github.com>
2024-05-03 00:02:13 +0200

    train better model

--- yolo/run.py
+++ yolo/run.py
@@ -1,20 +1,21 @@
 import time
-
 from ultralytics import YOLO
 import winsound
 
 
 def main(project_name: str) -> None:
+
     model_tuned = YOLO("yolov8s.pt")
     model_tuned.to("cuda")
+
     model_tuned.train(
         data='../NAPLab-LiDAR/data.yaml',
-        epochs=20,
+        epochs=300,
         patience=20,
         batch=4,
         imgsz=640,
         rect=False,
-        save_period=10,
+        save_period=20,
         cache='ram',
         device=0,
         project=project_name,
@@ -28,8 +29,6 @@ def main(project_name: str) -> None:
         shear=0,
         degrees=0
     )
-    model_tuned_metrics = model_tuned.val(data="../NAPLab-LiDAR/test.yaml")
-    print(model_tuned_metrics)
 
 
 if __name__ == '__main__':
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     try:
         main(n)
     finally:
-        for _ in range(10):
+        for _ in range(20):
             winsound.Beep(4000, 500)
             time.sleep(.15)
 
